5ed6e3973745499547cef2d35236d869ef6ef89c
Gilles DGLD <shomogo@hotmail.com>
2019-04-21 17:58:15 +0200

    add dogs page that lists dogs breeds

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Dogs from "./views/Dogs.vue";
 
 Vue.use(Router);
 
@@ -12,6 +13,11 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
+    },
+    {
+      path: "/dogs",
+      name: "dogs",
+      component: Dogs
     },
     {
       path: "/about",
---
+++ src/views/Dogs.vue
@@ -0,0 +1,28 @@
+<template>
+<ul id="example-1">
+  <li v-for="(subbreed, breed) in items">
+      {{ breed }} {{ subbreed }}
+  </li>
+</ul>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    name: "Dogs",
+    data () {
+        return {
+            items: null
+        }
+    },
+    mounted () {
+        axios
+        .get('https://dog.ceo/api/breeds/list/all')
+        .then(response => {console.log(response.data); this.items = response.data.message})
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
