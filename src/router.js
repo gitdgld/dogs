aaf882f8cbc6e6bef9efcf57992034c73f9b50f0
Gilles DGLD <shomogo@hotmail.com>
2019-05-06 17:23:22 +0200

    adding vuetify

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import About from "./views/About.vue";
 import Dogs from "./views/Dogs.vue";
 
 Vue.use(Router);
@@ -15,18 +16,14 @@ export default new Router({
       component: Home
     },
     {
+      path: "/about",
+      name: "about",
+      component: About
+    },
+    {
       path: "/dogs",
       name: "dogs",
       component: Dogs
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
 });
--- src/views/Dogs.vue
+++ src/views/Dogs.vue
@@ -1,28 +1,31 @@
 <template>
-<ul id="example-1">
-  <li v-for="(subbreed, breed) in items">
-      {{ breed }} {{ subbreed }}
-  </li>
-</ul>
+  <v-app>
+    <v-content>
+      <div class="dogs">
+        <ul id="example-1">
+          <li v-for="(subbreed, breed) in items">{{ breed }} {{ subbreed }}</li>
+        </ul>
+      </div>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-    name: "Dogs",
-    data () {
-        return {
-            items: null
-        }
-    },
-    mounted () {
-        axios
-        .get('https://dog.ceo/api/breeds/list/all')
-        .then(response => {console.log(response.data); this.items = response.data.message})
-    }
-}
+  name: "Dogs",
+  data() {
+    return {
+      items: null
+    };
+  },
+  mounted() {
+    axios.get("https://dog.ceo/api/breeds/list/all").then(response => {
+      console.log(response.data);
+      this.items = response.data.message;
+    });
+  }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
